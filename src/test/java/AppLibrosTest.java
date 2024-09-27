5bdd5e3d1a1ab63a1d0c60a8258094ac68f7b445
VOrtigosa <163462094+VOrtigosa@users.noreply.github.com>
2024-09-27 11:08:45 -0300

    Update AppLibrosTest.java

--- src/test/java/AppLibrosTest.java
+++ src/test/java/AppLibrosTest.java
@@ -17,6 +17,33 @@ public class AppLibrosTest {
         Assertions.assertEquals(1, 1);
     }
 
+     @Test
+    public void testAgregarLibro() {
+        // Configura el EntityManagerFactory para las pruebas (puedes usar un H2 in-memory para esto)
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("test-pu");
+        EntityManager em = emf.createEntityManager();
+        
+        // Crea un libro
+        Libro libro = new Libro();
+        libro.setNombre("El Principito");
+        libro.setAutor("Antoine de Saint-Exupéry");
+        libro.setPrecio(150L);
+
+        // Guarda el libro
+        RepoLibros repo = new RepoLibros(em);
+        em.getTransaction().begin();
+        repo.save(libro);
+        em.getTransaction().commit();
+
+        // Verifica que el libro se haya guardado correctamente
+        Collection<Libro> libros = repo.findAll();
+        Assertions.assertEquals(1, libros.size());
+        Assertions.assertEquals("El Principito", libros.iterator().next().getNombre());
+        
+        em.close();
+        emf.close();
+    }
+
   
 
 }
